ca6fbf1937b2dca0cc2c3e069a7d711bc81fcd8f
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-03-04 12:33:37 +0100

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
 import { Box } from '@mui/material';
-import Login from './pages/Login.jsx';
-import Home from './pages/Home.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
 
 function App({ spotifyApi }) {
 	return (
 		<Box className="App">
-			<Home />
+			<Dashboard spotifyApi={spotifyApi} />
 		</Box>
 	);
 }
