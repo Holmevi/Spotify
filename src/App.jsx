2dbc7995f709856465f0950b0570bc0f58f8e566
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-03-04 13:01:37 +0100

    Access token

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Box } from '@mui/material';
 import Dashboard from './components/Dashboard/Dashboard.jsx';
+import Login from './pages/Login.jsx';
 
 function App({ spotifyApi }) {
 	return (
 		<Box className="App">
+			<Login />
 			<Dashboard spotifyApi={spotifyApi} />
 		</Box>
 	);
