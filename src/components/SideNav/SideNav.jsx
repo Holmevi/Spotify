11bd962b34d23af8fe555d1f5fcc4223f5b312cd
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-03-05 14:51:16 +0100

    SideNav

---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import { Box, Divider } from "@mui/material";
+
+const SideNav = ({ spotifyApi, token }) => {
+    return <Box>SideNav</Box>;
+};
+
+export default SideNav;
