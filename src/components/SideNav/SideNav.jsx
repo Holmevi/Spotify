f098fd7decf7546af37e081711ea5db143d6d91e
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-03-05 15:12:35 +0100

    SideNav Style

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -2,7 +2,24 @@ import { useState, useEffect } from "react";
 import { Box, Divider } from "@mui/material";
 
 const SideNav = ({ spotifyApi, token }) => {
-    return <Box>SideNav</Box>;
+    return <Box sx={{
+        backgroundColor: 'Background.default',
+        width: 230,
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+
+    }}>
+        <Box p={3}>
+            <img src="public\Spotify_Logo.png" alt="Spotify Logo" width={'75%'} />
+        </Box>
+        <Box px={3} py={1}>
+            <Divider sx={{ backgroundColor: '#ffffff40' }} />
+        </Box>
+        <Box sx={{ overflowY: 'auto', flex: 1 }}>
+            Pop
+        </Box>
+    </Box>;
 };
 
 export default SideNav;
