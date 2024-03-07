842a01bda0d822468f85b453910637164dee8f40
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-03-07 18:09:35 +0100

    NavItem

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,36 +1,40 @@
-import { useState, useEffect } from "react";
-import { Box, Divider } from "@mui/material";
+import { useState, useEffect } from 'react';
+import { Box, Divider } from '@mui/material';
+import NavItem from '../NavItem/NavItem';
+import HomeIcon from '@mui/icons-material/Home';
 
 const SideNav = ({ spotifyApi, token }) => {
-    useEffect(() => {
-        async function getPlaylist() {
-            if(!spotifyApi) return;
+	useEffect(() => {
+		async function getPlaylist() {
+			if (!spotifyApi) return;
 
-            const data = await spotifyApi.getUserPlaylists();
-            console.log(data.body);
-        }
+			const data = await spotifyApi.getUserPlaylists();
+			console.log(data.body);
+		}
 
-        getPlaylist();
-    }, [spotifyApi, token]);
+		getPlaylist();
+	}, [spotifyApi, token]);
 
-    return <Box sx={{
-        backgroundColor: 'Background.default',
-        width: 230,
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column'
-
-    }}>
-        <Box p={3}>
-            <img src="public\Spotify_Logo.png" alt="Spotify Logo" width={'75%'} />
-        </Box>
-        <Box px={3} py={1}>
-            <Divider sx={{ backgroundColor: '#ffffff40' }} />
-        </Box>
-        <Box sx={{ overflowY: 'auto', flex: 1 }}>
-            Pop
-        </Box>
-    </Box>;
+	return (
+		<Box
+			sx={{
+				backgroundColor: 'Background.default',
+				width: 230,
+				height: '100%',
+				display: 'flex',
+				flexDirection: 'column'
+			}}
+		>
+			<Box p={3}>
+				<img src="public\Spotify_Logo.png" alt="Spotify Logo" width={'75%'} />
+			</Box>
+			<NavItem name="Home" Icon={HomeIcon} target="/" />
+			<Box px={3} py={1}>
+				<Divider sx={{ backgroundColor: '#ffffff40' }} />
+			</Box>
+			<Box sx={{ overflowY: 'auto', flex: 1 }}>Pop</Box>
+		</Box>
+	);
 };
 
 export default SideNav;
