24b05f63cead4c522ce585bac4c4b5a0afa5f923
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-03-16 17:53:48 +0100

    Play music in the player

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,6 +1,39 @@
 import { Box, Grid, Typography, Avatar } from '@mui/material';
+import { useEffect } from 'react';
 
-const Player = ({ spotifyApi }) => {
+const Player = ({ spotifyApi, token }) => {
+	useEffect(() => {
+		const script = document.createElement('script');
+		script.src = 'https://sdk.scdn.co/spotify-player.js';
+		script.async = true;
+
+		document.body.appendChild(script);
+
+		window.onSpotifyWebPlaybackSDKReady = () => {
+			const player = new window.Spotify.Player({
+				name: 'Techover Player',
+				getOAuthToken: (cb) => {
+					cb(token);
+				},
+				volume: 0.5
+			});
+
+			player.addListener('ready', ({ device_id }) => {
+				console.log('Ready with Device ID', device_id);
+			});
+
+			player.addListener('not_ready', ({ device_id }) => {
+				console.log('Device ID has gone offline', device_id);
+			});
+            
+            player.addListener('player_state_changed', (state) => {
+                console.log(state);
+            });
+
+			player.connect();
+		};
+	}, []);
+
 	return (
 		<Box>
 			<Grid
@@ -11,15 +44,15 @@ const Player = ({ spotifyApi }) => {
 					height: 100,
 					cursor: { xs: 'pointer', md: 'auto' },
 					width: '100%',
-                    borderTop: '1px solid #292929'
+					borderTop: '1px solid #292929'
 				}}
 			>
 				<Grid xs={12} md={4} items sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
-					<Avatar src={null} alt={null} variant='square' sx={{ width: 56, height: 56, marginRight: 2 }} />
-                    <Box>
-                        <Typography sx={{ color: 'text.primary', fontSize: 14 }}>Can´t take a joke</Typography>
-                        <Typography sx={{ color: 'text.secondary', fontSize: 10 }}>Drake</Typography>
-                    </Box>
+					<Avatar src={null} alt={null} variant="square" sx={{ width: 56, height: 56, marginRight: 2 }} />
+					<Box>
+						<Typography sx={{ color: 'text.primary', fontSize: 14 }}>Can´t take a joke</Typography>
+						<Typography sx={{ color: 'text.secondary', fontSize: 10 }}>Drake</Typography>
+					</Box>
 				</Grid>
 				<Grid
 					sx={{
