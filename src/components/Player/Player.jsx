1241a54434f21570bdaf92d3dda1317db45ac0a4
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-03-16 17:21:28 +0100

    Player style

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -3,9 +3,23 @@ import { Box, Grid, Typography, Avatar } from '@mui/material';
 const Player = ({ spotifyApi }) => {
 	return (
 		<Box>
-			<Grid container>
-				<Grid xs={12} md={4} items sx={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
-					Bild, Titel, Artist
+			<Grid
+				container
+				px={3}
+				sx={{
+					backgroundColor: 'background.paper',
+					height: 100,
+					cursor: { xs: 'pointer', md: 'auto' },
+					width: '100%',
+                    borderTop: '1px solid #292929'
+				}}
+			>
+				<Grid xs={12} md={4} items sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+					<Avatar src={null} alt={null} variant='square' sx={{ width: 56, height: 56, marginRight: 2 }} />
+                    <Box>
+                        <Typography sx={{ color: 'text.primary', fontSize: 14 }}>Can´t take a joke</Typography>
+                        <Typography sx={{ color: 'text.secondary', fontSize: 10 }}>Drake</Typography>
+                    </Box>
 				</Grid>
 				<Grid
 					sx={{
