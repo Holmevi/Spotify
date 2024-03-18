cd1589503f646e80fba6db9c62c0d4c2a173a268
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-03-18 13:39:33 +0100

    Icon button

--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -4,9 +4,21 @@ import { PlayArrow, SkipNext, SkipPrevious, Pause } from '@mui/icons-material';
 import { useState } from 'react';
 
 const PlayerControls = ({ is_paused, duration, progress, player }) => {
+	const skipStyle = { width: 28, height: 28 };
+	const playStyle = { width: 38, height: 38 };
 	return (
 		<Stack direction={'column'} spacing={2} justify="center" alignItems="center" sx={{ width: '100%' }}>
-			<Stack>Play-knappar</Stack>
+			<Stack spacing={1} direction="row" justifyContent={'center'} alignItems="center" sx={{ width: '100%' }}>
+				<IconButton size="small" sx={{ color: 'text.primary' }}>
+					<SkipPrevious sx={skipStyle} />
+				</IconButton>
+				<IconButton size="small" sx={{ color: 'text.primary' }}>
+					{is_paused ? <PlayArrow sx={playStyle} /> : <Pause sx={playStyle} />}
+				</IconButton>
+				<IconButton size="small" sx={{ color: 'text.primary' }}>
+					<SkipNext sx={skipStyle} />
+				</IconButton>
+			</Stack>
 			<Stack>Progress / Slider</Stack>
 		</Stack>
 	);
